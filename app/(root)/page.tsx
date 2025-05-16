c87a806542818aa03ef08ce7d8529d87821b27fc
Siddhant Singh <siddhantsingh19326@gmail.com>
2025-05-16 16:50:13 +0530

    adding global css for all standard components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>HomePage</div>
+    <div >
+      <h1 className="text-3xl bg-black underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
